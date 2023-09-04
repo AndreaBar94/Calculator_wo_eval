edb4ef568a83044b93cadcd37e86738967932a3c
AndreaBar94 <andreabarocchi@gmail.com>
2023-09-04 16:15:38 +0200

    update index.js

--- index.js
+++ index.js
@@ -1,49 +1,75 @@
-const calculator = document.querySelector(".calculator");
-const keys = calculator.querySelector(".calculator_buttons");
+const calculator = document.querySelector('.calculator');
+const keys = calculator.querySelector('.calculator_buttons');
+const display = document.querySelector('.calculator_display');
 
-keys.addEventListener("click", (e) => {
-	if (e.target.matches("button")) {
-		const key = e.target;
-		const action = key.dataset.action;
-		if (!action) {
-			console.log("number key!");
-		}
-		if (action === "add" || action === "subtract" || action === "multiply" || action === "divide") {
-			console.log("operator key!");
-			calculator.dataset.previousKeyType = "operator";
-		}
+let firstOperand = '';
+let operator = '';
+let waitingForSecondOperand = false;
 
-		if (action === "decimal") {
-			console.log("decimal key!");
-		}
-
-		if (action === "clear") {
-			console.log("clear key!");
-		}
-
-		if (action === "calculate") {
-			console.log("equal key!");
-		}
-	}
-});
-
-const display = document.querySelector(".calculator_display");
-
-keys.addEventListener("click", (e) => {
-	if (e.target.matches("button")) {
-		const key = e.target;
-		const action = key.dataset.action;
-		const keyContent = key.textContent;
-		const displayedNum = display.textContent;
-		if (!action) {
-			if (displayedNum === "0") {
-				display.textContent = keyContent;
+keys.addEventListener('click', (e) => {
+	const key = e.target;
+	const keyValue = key.textContent;
+	//casistiche per i bottoni premuti
+	if (key.tagName === 'BUTTON') {
+		if (keyValue === '+' || keyValue === '-' || keyValue === '*' || keyValue === '/') {
+			if (operator) {
+				return;
+			}
+			operator = keyValue;
+			firstOperand = display.textContent;
+			waitingForSecondOperand = true;
+		} else if (keyValue === '=') {
+			if (!operator) {
+				return;
+			}
+			if (waitingForSecondOperand) {
+				return;
+			}
+			const secondOperand = display.textContent;
+			firstOperand = calculate(firstOperand, operator, secondOperand);
+			operator = '';
+			waitingForSecondOperand = false;
+			display.textContent = firstOperand;
+		} else if (keyValue === 'AC') {
+			firstOperand = '';
+			operator = '';
+			waitingForSecondOperand = false;
+			display.textContent = '';
+		} else if (keyValue === '.') {
+			if (waitingForSecondOperand) {
+				return;
+			}
+			if (!firstOperand.includes('.')) {
+				firstOperand += '.';
+			}
+		} else {
+			if (waitingForSecondOperand) {
+				waitingForSecondOperand = false;
+				display.textContent = keyValue;
 			} else {
-				display.textContent = displayedNum + keyContent;
+				display.textContent = display.textContent === '0' ? keyValue : display.textContent + keyValue;
 			}
 		}
-		if (action === "decimal") {
-			display.textContent = displayedNum + ".";
-		}
 	}
 });
+
+function calculate(firstOperand, operator, secondOperand) {
+	firstOperand = parseFloat(firstOperand.replace(',', '.'));
+	secondOperand = parseFloat(secondOperand.replace(',', '.'));
+
+	switch (operator) {
+		case '+':
+			return (firstOperand + secondOperand).toString().replace('.', ','); // Reimposta la virgola come separatore decimale
+		case '-':
+			return (firstOperand - secondOperand).toString().replace('.', ',');
+		case '*':
+			return (firstOperand * secondOperand).toString().replace('.', ',');
+		case '/':
+			if (secondOperand === 0) {
+				return 'Errore';
+			}
+			return (firstOperand / secondOperand).toString().replace('.', ',');
+		default:
+			return secondOperand.toString().replace('.', ',');
+	}
+}
